d97a35b7ed39cac2eeb0a591ee795238c1af1aa1
CDR <1254688559@qq.com>
2019-11-15 21:16:58 +0800

    newcodegame1

---
+++ 分糖果/分糖果/Test.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main(int argc, char **argv) {
+
+	int n;
+	float sum1, sum2;
+	for (n = 11;; n++) {
+		sum1 = (n + 9) / 10.0;
+		sum2 = (9 * n + 171) / 100.0;
+		if (sum1 != (int)sum1)
+			continue;
+		if (sum2 != (int)sum2)
+			continue;
+		if (sum1 == sum2)
+			break;
+	}
+	printf("共有%d个学生\n将糖果分成了%d份", (int)(n / sum1), n);
+	printf("\n");
+
+	return 0;
+}
